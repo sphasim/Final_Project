f4c295a7abd91c4a09cb995081bc6e0d7ac8835a
Siphamandla <sphasim@gmail.com>
2016-04-18 15:36:11 -0600

    Add_Project

---
+++ app/src/main/java/edu/nmhu/ssd/trakz/MainActivity.java
@@ -0,0 +1,235 @@
+package edu.nmhu.ssd.trakz;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.MessageFormat;
+
+public class MainActivity extends AppCompatActivity{
+
+    LocationManager mLocManager;
+    LocationListener mLocListener;
+    GpsStatus.Listener gpsStatusListener;
+    Location lastLocation;
+    long lastFix;
+    boolean hasFix;
+
+    // TextViews
+    TextView tvLatitude;
+    TextView tvLongitude;
+    TextView tvAccuracy;
+    TextView tvTime;
+
+    final DecimalFormat accuracyFormat = new DecimalFormat("###.00");
+
+    private Handler handler = new Handler();
+    private Runnable updateUITask = new Runnable() {
+        public void run() {
+            updateDisplay();
+            handler.postDelayed(this, 100);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Context context = getApplicationContext();
+
+        // Setup TextViews
+        tvLatitude = (TextView)findViewById(R.id.latitudeText);
+        tvLongitude = (TextView)findViewById(R.id.longitudeText);
+        tvAccuracy = (TextView)findViewById(R.id.accuracyText);
+        tvTime = (TextView)findViewById(R.id.timeText);
+
+        hasFix = false;
+
+        mLocManager = (LocationManager)context.getSystemService(LOCATION_SERVICE);
+        boolean isGPSEnabled = mLocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (isGPSEnabled && mLocManager != null){
+
+            // Create the LocationListener
+            mLocListener = new LocationListener(){
+                public void onLocationChanged(Location loc){
+                    updateLocation(loc);
+                }
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+                public void onProviderEnabled(String provider) {}
+                public void onProviderDisabled(String provider) {}
+            };
+
+            // Create the GPS status listener
+            gpsStatusListener = new GpsStatus.Listener(){
+                public void onGpsStatusChanged(int event){
+                    switch(event){
+                        case GpsStatus.GPS_EVENT_STARTED:
+                            System.out.println("GPS_EVENT_STARTED");
+                            break;
+                        case GpsStatus.GPS_EVENT_STOPPED:
+                            System.out.println("GPS_EVENT_STOPPED");
+                            break;
+                        case GpsStatus.GPS_EVENT_FIRST_FIX:
+                            System.out.println("GPS_EVENT_FIRST_FIX");
+                            hasFix = true;
+                            break;
+                        case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+                            System.out.println("GPS_EVENT_SATELLITE_STATUS");
+                            if (lastLocation != null) {
+                                hasFix = (System.currentTimeMillis() - lastFix) < 2000;
+                            }
+
+                            if (hasFix) {
+                                System.out.println("HAS FIX");
+                            }
+                            else {
+                                System.out.println("NO FIX");
+                            }
+                    }
+                }
+            };
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                lastLocation = mLocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            } else {
+//                ActivityCompat.requestPermissions(thisActivity,
+//                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                        ACCESS_FINE_LOCATION);
+
+            }
+//                lastLocation = mLocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastLocation != null){
+                lastFix = lastLocation.getTime();
+            }
+
+            handler.post(updateUITask);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            lastLocation = mLocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        } else {
+//                ActivityCompat.requestPermissions(thisActivity,
+//                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                        ACCESS_FINE_LOCATION);
+
+        }
+        mLocManager.removeUpdates(mLocListener);
+        mLocManager.removeGpsStatusListener(gpsStatusListener);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            lastLocation = mLocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        } else {
+//                ActivityCompat.requestPermissions(thisActivity,
+//                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                        ACCESS_FINE_LOCATION);
+
+        }
+        mLocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocListener);
+        mLocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocListener);
+        mLocManager.addGpsStatusListener(gpsStatusListener);
+    }
+
+    public void shareLocation (View view){
+        if (lastLocation == null){
+            return;
+        }
+
+        String link = formatLocation("http://maps.google.com/?q={0},{1}", lastLocation);
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, link);
+        intent.setType("text/plain");
+        startActivity(Intent.createChooser(intent, getResources().getText(R.string.send_via)));
+    }
+
+    public void viewLocation (View view){
+        if (lastLocation == null){
+            return;
+        }
+
+        String uri = formatLocation("geo:{0},{1}?q={0},{1}", lastLocation);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        startActivity(Intent.createChooser(intent, getResources().getText(R.string.view_via)));
+    }
+    public void updateLocation(Location location){
+        //TODO: Check if new location is 'better'
+        lastLocation = location;
+        lastFix = location.getTime();
+        updateDisplay();
+    }
+
+    public void updateDisplay(){
+
+        if (lastLocation != null){
+            long timeElapsed = System.currentTimeMillis() - lastFix;
+            tvTime.setText(stringifyTime(timeElapsed / 1000));
+
+            tvLatitude.setText("" + lastLocation.getLatitude());
+            tvLongitude.setText("" + lastLocation.getLongitude());
+            tvAccuracy.setText(accuracyFormat.format(lastLocation.getAccuracy()) + "m");
+        }
+        else{
+            tvLatitude.setText("N/A");
+            tvLongitude.setText("N/A");
+            tvAccuracy.setText("N/A");
+            tvTime.setText("N/A");
+        }
+
+    }
+
+    private String formatLocation(String s, Location l){
+        // Hack to get around MessageFormat precision weirdness
+        return MessageFormat.format(s, "" + l.getLatitude(), "" + l.getLongitude(), "" + l.getAccuracy());
+    }
+
+    public static String stringifyTime(long seconds) {
+
+        int hours = (int)(seconds/3600);
+        seconds -= hours * 3600;
+        int minutes = (int)(seconds/60);
+        seconds -= minutes * 60;
+
+        // Fix negative values (rounding)
+        seconds = Math.max(0, seconds);
+
+        return MessageFormat.format("{0,number,00}:{1,number,00}:{2,number,00}", hours, minutes, seconds);
+    }
+}
